5b8ed52b95eb6493815c59f48999bd315cc9eb7f
Mod <118411138+NapacholKa@users.noreply.github.com>
2023-01-27 11:21:07 +0700

    Practice_2 [28-Jan-2023]

---
+++ Invoice.java
@@ -0,0 +1,46 @@
+public class Invoice {
+	private int id;
+	Customer customer;
+	double amount;
+	public Invoice(int id,Customer customer,double amount) {
+		this.id = id;
+		this.customer = customer;
+		this.amount =amount;
+	} 
+	public int getID() {
+		return id;
+	}
+	public Customer getCustomer() {
+		return customer  ;
+		
+	}
+	public void  setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public int getCustomerID() {
+		return customer.getID();
+	}
+	public String getCustomerName() {
+		return customer.getName();
+		}
+	public int getCustomerDiscount() {
+		return customer.getDiscount();
+	}
+	public double getAmountAfterDiscount() {
+		return getAmount()-getAmount()*(getCustomerDiscount()/100);
+	}
+	public String toString(){
+		return "Invoice[id="+id+",customer="+customer.getName()+
+				"("+customer.getID()+")("+customer.getDiscount()+
+				"%),amount="+getAmount()+"]";
+	}
+	
+	
+	
+}
